e16eb60bd220880148668c361bcb172f13cd96e4
valery <v.a.kiselyov@gmail.com>
2024-11-17 20:25:28 +0300

    Created Dockerfile.

---
+++ provision.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+apt install docker.io
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+docker --version
+docker compose version
+
+echo "1. Create file: 'vim ./.env'"
