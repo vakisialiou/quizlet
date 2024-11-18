bdb723e8329a2b2b27fd46a757349f18bad3d542
valery <v.a.kiselyov@gmail.com>
2024-11-18 16:51:49 +0300

    Created docker-compose.yml.

--- docker/cert.sh
+++ docker/cert.sh
@@ -6,7 +6,7 @@ CERT_PATH="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
 # Check if the SSL certificate exists
 if [ ! -f "$CERT_PATH" ]; then
     echo "SSL certificate not found. Generating new certificate for $DOMAIN..."
-    certbot --nginx --non-interactive --agree-tos -m admin@$DOMAIN -d $DOMAIN
+    certbot --nginx --agree-tos --preferred-challenges http -d $DOMAIN
 else
     echo "SSL certificate already exists. Checking if renewal is needed..."
     certbot renew --quiet
