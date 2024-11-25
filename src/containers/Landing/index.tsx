b0ef1dbc8ab864352631bd521185a8c44e85ada8
valery <v.a.kiselyov@gmail.com>
2024-11-25 20:40:45 +0300

    Updated landing localization.

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -9,7 +9,6 @@ import { useSelector } from 'react-redux'
 import { signIn } from 'next-auth/react'
 import { Session } from 'next-auth'
 import { memo } from 'react'
-
 import clsx from 'clsx'
 
 function Landing() {
@@ -41,149 +40,166 @@ function Landing() {
             Beta
           </div>
 
-          <div className="flex flex-col text-center text-white px-6 max-w-[900px]">
-            <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-8">
-              {t('mainTitle')}
-            </h1>
-            <p className="text-lg md:text-xl mb-4">
-              {t('mainDesc1')}
-            </p>
-
-            <p className="text-lg md:text-xl mb-8">
-              {t('mainDesc2')}
-            </p>
-
-            <div className="flex justify-center">
-              <Button
-                className="px-6 gap-2 font-medium"
-                size={ButtonSize.H12}
-                skin={ButtonSkin.WHITE_100}
-                onClick={async () => {
-                  if (!session) {
-                    await signIn('google')
-                  } else {
-                    route.push('/private')
-                  }
-                }}
-              >
-                {!session &&
-                  <>
-                    <SVGGoogle
-                      width={24}
-                      height={24}
-                    />
-                    {t('mainButtonSignIn')}
-                  </>
-                }
-
-                {session && t('mainButtonGoTo')}
-              </Button>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <section id="features" className="py-20 bg-white text-gray-700">
-        <div className="flex flex-col items-center max-w-6xl mx-auto px-6">
-          <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 max-w-[320px] md:max-w-full">
-            {t('section1Title')}
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="p-6 bg-gray-50 shadow-md rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">{t('section1Block1Title')}</h3>
-              <p>{t('section1Block1Text')}</p>
-            </div>
-            <div className="p-6 bg-gray-50 shadow-md rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">{t('section1Block2Title')}</h3>
-              <p>{t('section1Block2Text')}</p>
-            </div>
-            <div className="p-6 bg-gray-50 shadow-md rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">{t('section1Block3Title')}</h3>
-              <p>{t('section1Block3Text')}</p>
-            </div>
-            <div className="p-6 bg-gray-50 shadow-md rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">{t('section1Block4Title')}</h3>
-              <p>{t('section1Block4Text')}</p>
-            </div>
-            <div className="p-6 bg-gray-50 shadow-md rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">{t('section1Block5Title')}</h3>
-              <p>{t('section1Block5Text')}</p>
-            </div>
-            <div className="p-6 bg-gray-50 shadow-md rounded-lg">
-              <h3 className="text-xl font-semibold mb-4">{t('section1Block6Title')}</h3>
-              <p>{t('section1Block6Text')}</p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section
-        id="how-it-works"
-        className="relative bg-fixed bg-cover"
-        style={{ backgroundImage: `url('/images/bg-how-it-works.avif')`, backgroundPosition: 'center' }}
-      >
-        <div className="bg-black bg-opacity-70 py-36 px-6">
-          <div className="max-w-5xl mx-auto">
-            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
-              {t('section2Title', { appName })}
-            </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-gray-400">
-              <div className="p-6 bg-gray-900/50 border border-gray-400/50 shadow-lg rounded-lg">
-                <h3 className="text-xl font-semibold mb-4">{t('section2Block1Title')}</h3>
-                <p>{t('section2Block1Text')}</p>
-              </div>
-              <div className="p-6 bg-gray-900/50 border border-gray-400/50 shadow-lg rounded-lg">
-                <h3 className="text-xl font-semibold mb-4">{t('section2Block2Title')}</h3>
-                <p>{t('section2Block2Text')}</p>
-              </div>
-              <div className="p-6 bg-gray-900/50 border border-gray-400/50 shadow-lg rounded-lg">
-                <h3 className="text-xl font-semibold mb-4">{t('section2Block3Title')}</h3>
-                <p>{t('section2Block3Text')}</p>
+          {session &&
+            <div className="flex flex-col text-center text-white px-6 max-w-[900px]">
+              <div className="flex justify-center">
+                <Button
+                  size={ButtonSize.H12}
+                  skin={ButtonSkin.WHITE_100}
+                  onClick={() => route.push('/private')}
+                  className="px-6 gap-2 font-medium text-nowrap"
+                >
+                  {t('mainButtonGoTo')}
+                </Button>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
+          }
 
-      <section id="cta" className="py-20 bg-gradient-to-r from-blue-500 to-blue-600 text-white">
-        <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl md:text-4xl font-bold mb-8">{t('section3Title', { appName })}</h2>
-          <p className="text-lg md:text-xl mb-8">{t('section3Text')}</p>
+          {!session &&
+            <div className="flex flex-col text-center text-white px-6 max-w-[900px]">
+              <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-12">
+                {t('mainTitle')}
+              </h1>
+              <p
+                className="text-lg md:text-xl mb-12"
+                dangerouslySetInnerHTML={{__html: t('mainDesc')}}
+              />
 
-          <div className="flex justify-center gap-8">
-            <Button
-              className="px-6 gap-2 font-medium"
-              size={ButtonSize.H12}
-              skin={ButtonSkin.WHITE_100}
-              onClick={async () => {
-                if (!session) {
-                  await signIn('google')
-                } else {
-                  route.push('/private')
-                }
-              }}
-            >
-              {!session &&
-                <>
+              <div className="flex justify-center">
+                <Button
+                  size={ButtonSize.H12}
+                  skin={ButtonSkin.WHITE_100}
+                  onClick={async () => await signIn('google')}
+                  className="px-6 gap-2 font-medium text-nowrap"
+                >
                   <SVGGoogle
                     width={24}
                     height={24}
                   />
-                  {t('section3ButtonSignIn')}
-                </>
-              }
-
-              {session && t('section3ButtonGoTo')}
-            </Button>
-          </div>
+                  {t('mainButtonSignIn')}
+                </Button>
+              </div>
+            </div>
+          }
         </div>
-      </section>
+      </header>
 
-      <footer className="bg-gray-900 text-gray-400 py-6">
-        <div className="max-w-6xl mx-auto text-center">
-          <p>{t('footer', { appName })}</p>
-        </div>
-      </footer>
+      {!session &&
+        <>
+          <section id="features" className="py-20 bg-white text-gray-700">
+            <div className="flex flex-col items-center max-w-6xl mx-auto px-6">
+              <h2
+                className="text-3xl md:text-4xl font-bold text-center mb-12 max-w-[320px] md:max-w-full">
+                {t('section1Title', {appName})}
+              </h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section1Block1Title')}</h3>
+                  <p>{t('section1Block1Text')}</p>
+                </div>
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section1Block2Title')}</h3>
+                  <p>{t('section1Block2Text')}</p>
+                </div>
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section1Block3Title')}</h3>
+                  <p>{t('section1Block3Text')}</p>
+                </div>
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section1Block4Title')}</h3>
+                  <p>{t('section1Block4Text')}</p>
+                </div>
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section1Block5Title')}</h3>
+                  <p>{t('section1Block5Text')}</p>
+                </div>
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section1Block6Title')}</h3>
+                  <p>{t('section1Block6Text')}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section
+            id="how-it-works"
+            className="relative bg-fixed bg-cover"
+            style={{
+              backgroundImage: `url('/images/bg-how-it-works.avif')`,
+              backgroundPosition: 'center'
+            }}
+          >
+            <div className="bg-black bg-opacity-70 py-36 px-6">
+              <div className="max-w-5xl mx-auto">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                  {t('section2Title', {appName})}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-gray-400">
+                  <div className="p-6 bg-gray-900/50 border border-gray-400/50 shadow-lg rounded-lg">
+                    <h3 className="text-xl font-semibold mb-4">{t('section2Block1Title')}</h3>
+                    <p>{t('section2Block1Text')}</p>
+                  </div>
+                  <div className="p-6 bg-gray-900/50 border border-gray-400/50 shadow-lg rounded-lg">
+                    <h3 className="text-xl font-semibold mb-4">{t('section2Block2Title')}</h3>
+                    <p>{t('section2Block2Text')}</p>
+                  </div>
+                  <div className="p-6 bg-gray-900/50 border border-gray-400/50 shadow-lg rounded-lg">
+                    <h3 className="text-xl font-semibold mb-4">{t('section2Block3Title')}</h3>
+                    <p>{t('section2Block3Text')}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="simulators" className="py-20 bg-white text-gray-700">
+            <div className="flex flex-col items-center max-w-6xl mx-auto px-6">
+              <h2
+                className="text-3xl md:text-4xl font-bold text-center mb-12 max-w-[320px] md:max-w-full"
+                dangerouslySetInnerHTML={{__html: t('section3Title', {appName})}}
+              />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section3Block1Title')}</h3>
+                  <p>{t('section3Block1Text')}</p>
+                </div>
+                <div className="p-6 bg-gray-50 shadow-md rounded-lg">
+                  <h3 className="text-xl font-semibold mb-4">{t('section3Block2Title')}</h3>
+                  <p>{t('section3Block2Text')}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="cta" className="py-20 bg-gradient-to-r from-blue-500 to-blue-600 text-white">
+            <div className="flex flex-col items-center max-w-4xl mx-auto text-center">
+              <h2 className="text-3xl md:text-4xl font-bold mb-8">{t('section4Title', {appName})}</h2>
+              <p className="text-lg md:text-xl mb-8 max-w-[600px]">{t('section4Text')}</p>
+
+              <div className="flex justify-center gap-8">
+                <Button
+                  size={ButtonSize.H12}
+                  skin={ButtonSkin.WHITE_100}
+                  className="px-6 gap-2 font-medium"
+                  onClick={async () => await signIn('google')}
+                >
+                  <SVGGoogle
+                    width={24}
+                    height={24}
+                  />
+                  {t('section4ButtonSignIn')}
+                </Button>
+              </div>
+            </div>
+          </section>
+
+          <footer className="bg-gray-900 text-gray-400 py-6">
+            <div className="max-w-6xl mx-auto text-center">
+              <p>{t('footer', {appName})}</p>
+            </div>
+          </footer>
+        </>
+      }
     </ContentPage>
   )
 }
