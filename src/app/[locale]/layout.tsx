0c9fc82da06c7a38fa08adc02d1f4aaa810cb485
valery <v.a.kiselyov@gmail.com>
2024-12-08 16:31:59 +0300

    Updated sitemap.xml.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,7 +26,7 @@ const geistMono = localFont({
 
 export async function generateMetadata({params}: { params: Promise<{ locale: LanguageEnums }> }) {
   const { locale } = await params
-  const t = await getTranslations({ locale, namespace: 'Metadata'})
+  const t = await getTranslations({ locale, namespace: 'Metadata' })
 
   const baseUrl = 'https://quizerplay.com'
 
@@ -35,19 +35,19 @@ export async function generateMetadata({params}: { params: Promise<{ locale: Lan
     keywords: t('keywords'),
     description: t('description'),
     icons: [
-      { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/icons/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '96x96', url: '/icons/favicon-96x96.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/icons/favicon-16x16.png' },
-      { rel: 'apple-touch-icon', sizes: '57x57', url: '/icons/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', url: '/icons/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', url: '/icons/apple-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', url: '/icons/apple-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', url: '/icons/apple-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', url: '/icons/apple-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', url: '/icons/apple-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', url: '/icons/apple-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', url: '/icons/apple-icon-180x180.png' },
-      { rel: 'manifest', url: '/manifest.json' },
+      {rel: 'icon', type: 'image/png', sizes: '32x32', url: '/icons/favicon-32x32.png'},
+      {rel: 'icon', type: 'image/png', sizes: '96x96', url: '/icons/favicon-96x96.png'},
+      {rel: 'icon', type: 'image/png', sizes: '16x16', url: '/icons/favicon-16x16.png'},
+      {rel: 'apple-touch-icon', sizes: '57x57', url: '/icons/apple-icon-57x57.png'},
+      {rel: 'apple-touch-icon', sizes: '60x60', url: '/icons/apple-icon-60x60.png'},
+      {rel: 'apple-touch-icon', sizes: '72x72', url: '/icons/apple-icon-72x72.png'},
+      {rel: 'apple-touch-icon', sizes: '76x76', url: '/icons/apple-icon-76x76.png'},
+      {rel: 'apple-touch-icon', sizes: '114x114', url: '/icons/apple-icon-114x114.png'},
+      {rel: 'apple-touch-icon', sizes: '120x120', url: '/icons/apple-icon-120x120.png'},
+      {rel: 'apple-touch-icon', sizes: '144x144', url: '/icons/apple-icon-144x144.png'},
+      {rel: 'apple-touch-icon', sizes: '152x152', url: '/icons/apple-icon-152x152.png'},
+      {rel: 'apple-touch-icon', sizes: '180x180', url: '/icons/apple-icon-180x180.png'},
+      {rel: 'manifest', url: '/manifest.json'},
     ],
     alternates: {
       canonical: `${baseUrl}/${locale}`,
@@ -63,9 +63,13 @@ export default async function RootLayout({
  children,
  params
 }:
-  Readonly<{ children: React.ReactNode, params: Promise<{ locale: LanguageEnums }> }>
+ Readonly<{
+   children: React.ReactNode,
+   params: Promise<{ locale: LanguageEnums }>
+ }
+>
 ) {
-  const { locale } = await params
+  const {locale} = await params
   if (!routing.locales.includes(locale)) {
     notFound()
   }
