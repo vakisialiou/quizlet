bddc51aa0e4a6633c6b73d91847dbdc60cb9e041
valery <v.a.kiselyov@gmail.com>
2025-01-20 18:17:11 +0300

    Removed prisma logs.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -37,7 +37,7 @@ export const transaction = <T>(prisma: PrismaClient, callback: (entry: Prisma.Tr
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
+  log: process.env.NODE_ENV !== 'production' ? ['query', 'info', 'warn', 'error'] : [],
 })
 
 if (process.env.NODE_ENV !== 'production') {
