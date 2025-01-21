ed1b2078c356cba1c1a875e33303c9fd68b60525
valery <v.a.kiselyov@gmail.com>
2025-01-21 11:26:42 +0300

    Updated store.

--- src/middleware/private.ts
+++ src/middleware/private.ts
@@ -1,9 +1,9 @@
 import {NextRequest, NextResponse} from 'next/server'
 import { getToken } from 'next-auth/jwt'
-import { config } from '@config'
+import { config, ENV } from '@config'
 
 export async function privateMiddleware(req: NextRequest) {
-  const token = await getToken({ req, secret: config.oauth.secret, secureCookie: false, raw: true })
+  const token = await getToken({ req, secret: config.oauth.secret, secureCookie: config.server.env === ENV.PROD, raw: true })
 
   if (!token) {
     return NextResponse.redirect(new URL('/', req.url))
@@ -14,7 +14,7 @@ export async function privateMiddleware(req: NextRequest) {
 
 
 export async function privateApiMiddleware(req: NextRequest) {
-  const token = await getToken({ req, secret: config.oauth.secret, secureCookie: false, raw: true })
+  const token = await getToken({ req, secret: config.oauth.secret, secureCookie: config.server.env === ENV.PROD, raw: true })
 
   if (!token) {
     return new NextResponse(null, { status: 401 })
