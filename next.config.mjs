fc71fe078954f5f6810e9109b4bd7ace45e66e17
valery <v.a.kiselyov@gmail.com>
2025-02-13 10:57:38 +0300

    Added yandex metric.

--- next.config.mjs
+++ next.config.mjs
@@ -44,10 +44,10 @@ const nextConfig = {
           //   key: 'Content-Security-Policy',
           //   value: "default-src 'self'; script-src 'self' ",
           // },
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru; img-src 'self' https://mc.yandex.ru;"
-          },
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru; img-src 'self' https://mc.yandex.ru;"
+          // },
           // {
           //   key: 'Content-Security-Policy',
           //   value: "default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src 'self' https://mc.yandex.ru;"
