c1c76c58d31e219b57f85c360149a6a3910317a3
valery <v.a.kiselyov@gmail.com>
2025-02-16 16:53:37 +0300

    Added schema.org.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,10 @@
 import { generateMetaAlternates } from '@helper/meta'
 import { getTranslations } from 'next-intl/server'
 import { ViewportEnums } from '@lib/viewport'
-import { LanguageEnums } from '@i18n/routing'
+import { LanguageEnums,  } from '@i18n/routing'
 import { headers } from 'next/headers'
 import Main from '@containers/Main'
+import Script from 'next/script'
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: LanguageEnums }>}) {
   const { locale } = await params
@@ -32,11 +33,42 @@ export default async function Page(
 
   const viewport = headerList.get('x-viewport') as ViewportEnums
 
+  const tl = await getTranslations({ locale, namespace: 'Landing' })
+  const tn = await getTranslations({ locale, namespace: 'NavMenu' })
   return (
-    <Main
-      locale={locale}
-      viewport={viewport}
-      isExt={ext === '1'}
-    />
+    <>
+      <Main
+        locale={locale}
+        viewport={viewport}
+        isExt={ext === '1'}
+      />
+      <Script
+        id="schema-org"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'WebPage',
+          'name': tl('schemaOrgName'),
+          'description': tl('schemaOrgDescription'),
+          'url': `https://quizerplay.com/${locale}`,
+          'breadcrumb': {
+            '@type': 'BreadcrumbList',
+            'itemListElement': [
+              {
+                '@type': 'ListItem',
+                'position': 1,
+                'name': tn('home'),
+                'item': `https://quizerplay.com/${locale}`
+              }
+            ]
+          },
+          'primaryImageOfPage': {
+            '@type': 'ImageObject',
+            'contentUrl': 'https://quizerplay.com/images/1200x630.webp'
+          }
+        })}}
+      />
+    </>
   )
 }
